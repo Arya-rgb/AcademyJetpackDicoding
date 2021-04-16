b15ab08287dd790e0bea410d0490e969dc48f1c7
DESKTOP-5VPIR8G\Dicky arya pratama <dickyarya.personal@gmail.com>
2021-04-16 22:38:59 +0800

    dicoding baru sampai langkah 7 , santuy haha

--- app/src/main/java/com/project/academy/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/project/academy/detail/DetailCourseActivity.kt
@@ -5,22 +5,38 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.project.academy.R
+import com.project.academy.databinding.ActivityDetailCourseBinding
+import com.project.academy.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
+    /*
+   Every class can implement a companion object,
+   which is an object that is common to all instances of that class.
+   can also be used for passing data from another activity
+     */
     companion object {
         const val EXTRA_COURSE = "extra_course"
     }
 
+    //viewbinding implementation
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_course)
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+        //view binding implementation for main layout
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        //view binding implementation for detail content
+        detailContentBinding = activityDetailCourseBinding.detailContent
+
+        //set content view with binding's root
+        setContentView(activityDetailCourseBinding.root)
+
+        //set support action bar for this layout
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
+        //will make the icon clickable and add the < at the left of the icon.
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
